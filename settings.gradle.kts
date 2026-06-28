4e2f0090c85c278caf50c14bedb87e85224f0f7b
Auto Build <ci@autobuild.local>
2026-06-28 04:29:05 +0000

    fix: 添加Xposed和JitPack仓库解决依赖问题

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://api.xposed.info/") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
